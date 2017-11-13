fe9a173f2b3b423ef9a083dc44b737f667de1fab
CharlesZ-Chen <charleszhuochen@gmail.com>
2017-11-12 21:42:25 -0500

    Force use graph strategy for dataflow type system.

--- src/dataflow/solvers/general/DataflowSolverEngine.java
+++ src/dataflow/solvers/general/DataflowSolverEngine.java
@@ -1,5 +1,6 @@
 package dataflow.solvers.general;
 
+import checkers.inference.InferenceMain;
 import checkers.inference.solver.SolverEngine;
 import checkers.inference.solver.strategy.SolveStrategy;
 
@@ -24,4 +25,12 @@ public class DataflowSolverEngine extends SolverEngine {
         }
     }
 
+    @Override
+    protected void sanitizeConfiguration() {
+        if (!"graph".equals(strategyName)) {
+            InferenceMain.getInstance().logger.warning("Dataflow type system must use graph solve strategy."
+                    + "Change strategy from " + strategyName + " to graph.");
+            strategyName = "graph";
+        }
+    }
 }

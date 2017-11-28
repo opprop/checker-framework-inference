ec5b89e3f961876a79204b8f9c739aa53f5f5653
CharlesZ-Chen <charleszhuochen@gmail.com>
2017-11-28 14:47:49 -0500

    Add javadoc for SolverFactory.

--- src/checkers/inference/solver/backend/SolverFactory.java
+++ src/checkers/inference/solver/backend/SolverFactory.java
@@ -8,8 +8,27 @@ import checkers.inference.model.Constraint;
 import checkers.inference.model.Slot;
 import checkers.inference.solver.frontend.Lattice;
 import checkers.inference.solver.util.SolverOptions;
-import checkers.inference.util.ConstraintVerifier;
+import checkers.inference.solver.SolverEngine;
 
+/**
+ * Factory class for creating an underlying solver.
+ *
+ * Note: subclass of this interface should have a zero parameter
+ * constructor, and should follow the naming convention to let {@code SolverEngine}
+ * reflectively load the subclass instance.
+ *
+ * Naming convention of solver factory for underlying solvers:
+ *
+ * Package name should be: checkers.inference.solver.backend.[(all lower cases)underlying solver name]
+ * Under this package, create a subclass named: [underlying solver name]SolverFactory.
+ *
+ * E.g. For MaxSat solver:
+ *
+ * Package name: checkers.inference.solver.backend.maxsat
+ * Class name: MaxSatSolverFactory
+ *
+ * @see SolverEngine#createSolverFactory()
+ */
 public interface SolverFactory {
 
     Solver<?> createSolver(SolverOptions solverOptions,

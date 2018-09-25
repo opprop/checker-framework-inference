d2becd08a25d3cf1ce00baa4849ef6e8340a564a
Jeff Luo <j36luo@uwaterloo.ca>
2018-09-24 21:25:57 -0400

    add output on when each solver is run

--- testing/dataflowexample/travis-test.sh
+++ testing/dataflowexample/travis-test.sh
@@ -14,14 +14,17 @@ export PATH=$LINGELING:$AFU/scripts:$PATH
 ## Build libs for test
 (cd $WORKING_DIR && ant compile-libs)
 
+echo -e "\nRunning DataflowSolver\n"
 # test using basic dataflow solver
 $WORKING_DIR/runDataflowSolver.sh
 $WORKING_DIR/cleanup.sh
 
+echo -e "\nRunning MaxSatSolver\n"
 # test using maxsat (internal) solver
 $WORKING_DIR/runMaxSatSolver.sh
 $WORKING_DIR/cleanup.sh
 
+echo -e "\nRunning LingelingSolver\n"
 # test using lingeling (external) solver
 $WORKING_DIR/runLingelingSolver.sh
 $WORKING_DIR/cleanup.sh

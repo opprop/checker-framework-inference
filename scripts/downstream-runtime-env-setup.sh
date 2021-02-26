651a42ece53b26bf99b2d679c329c9870a32e572
d367wang <d367wang@uwaterloo.ca>
2021-02-26 00:03:11 +0000

    tweaks

--- scripts/downstream-runtime-env-setup.sh
+++ scripts/downstream-runtime-env-setup.sh
@@ -2,17 +2,20 @@
 
 #
 # This script setups the runtime environmental variables necessary to
-# run type inference.
+# run downstream type inference.
 #
-# Should be executed first by downstream inference starter script.
-# The following environmental variable are required to be specified in
+# Should be imported first by downstream inference starter script.
+# The following environmental variable should be specified in
 # downstream starter:
 # 1. checker & solver class
 # 2. classpath
+# 3. other inference arguments if necessary
 #
 
 ROOT=$(cd $(dirname "$0")/.. && pwd)
 CFI=$ROOT/checker-framework-inference
 AFU=$ROOT/annotation-tools/annotation-file-utilities
-export PATH=$AFU/scripts:$PATH
+Z3=$ROOT/z3/bin
+
+PATH=$AFU/scripts:$PATH
 

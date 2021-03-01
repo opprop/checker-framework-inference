65d882096d56a86b1ec9dd30e7a5732bb3a7d67b
d367wang <d367wang@uwaterloo.ca>
2021-03-01 15:53:57 +0000

    add lingeling to PATH, remove 'downstream'

---
+++ scripts/runtime-env-setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This script setups the common runtime environment variables necessary 
+# to run type inference.
+#
+# Should be imported firstly by any inference launch script or from 
+# the command line.
+#
+# The following environment variables should be specified separately 
+# in the inference launch script:
+# 1. checker & solver class
+# 2. classpath
+# 3. other inference arguments if necessary
+
+ROOT=$(cd $(dirname "$0")/.. && pwd)
+CFI=$ROOT/checker-framework-inference
+AFU=$ROOT/annotation-tools/annotation-file-utilities
+Z3=$ROOT/z3/bin
+LINGELING=$CFI/lib/lingeling
+
+export PATH=$AFU/scripts:$Z3:$LINGELING:$PATH

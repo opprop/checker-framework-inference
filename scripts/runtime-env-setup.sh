cdf93282d42d18d4547977d53d61a0e9eec99dc5
d367wang <d367wang@uwaterloo.ca>
2021-03-09 17:53:30 +0000

    replace env-setting command in ci-test.sh by importing common script

--- scripts/runtime-env-setup.sh
+++ scripts/runtime-env-setup.sh
@@ -12,7 +12,11 @@
 # 2. classpath
 # 3. other inference arguments if necessary
 
-ROOT=$(cd $(dirname "$0")/.. && pwd)
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Set ROOT as the home directory of current user
+ROOT=$(cd $SCRIPTDIR/../../ && pwd)
+
 CFI=$ROOT/checker-framework-inference
 AFU=$ROOT/annotation-tools/annotation-file-utilities
 Z3=$ROOT/z3/bin

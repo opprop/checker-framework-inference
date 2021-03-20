36adc3bf17441f0547ea038fd3006531e330393f
d367wang <d367wang@uwaterloo.ca>
2021-03-20 02:42:20 +0000

    not override checkConstructorResult in InferenceVisitor

--- src/checkers/inference/InferenceVisitor.java
+++ src/checkers/inference/InferenceVisitor.java
@@ -17,7 +17,6 @@ import org.checkerframework.framework.util.AnnotatedTypes;
 import org.checkerframework.javacutil.AnnotationBuilder;
 import org.checkerframework.javacutil.AnnotationUtils;
 import org.checkerframework.javacutil.BugInCF;
-import org.checkerframework.javacutil.TreeUtils;
 
 import java.lang.annotation.Annotation;
 import java.util.HashSet;
@@ -849,14 +848,4 @@ public class InferenceVisitor<Checker extends InferenceChecker,
         return new InferenceValidator(checker, this, atypeFactory);
     }
     
-    @Override
-    // Do NOT perform this check until issue #218 is resolved
-    protected void checkConstructorResult(
-            AnnotatedExecutableType constructorType, ExecutableElement constructorElement) {
-        if (infer) {
-            AnnotatedTypeMirror returnType = constructorType.getReturnType();
-            final AnnotatedDeclaredType classType  = atypeFactory.getAnnotatedType(TreeUtils.enclosingClass(getCurrentPath()));
-            areEqual(returnType, classType, "", getCurrentPath().getLeaf());
-        }
-    }
 }

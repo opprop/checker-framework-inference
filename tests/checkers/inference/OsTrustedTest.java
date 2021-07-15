fad7c628a7df32368e51b2f12de623057487c62a
d367wang <d367wang@uwaterloo.ca>
2021-07-15 13:12:35 -0400

    change back to max2type solver

--- tests/checkers/inference/OsTrustedTest.java
+++ tests/checkers/inference/OsTrustedTest.java
@@ -1,7 +1,6 @@
 package checkers.inference;
 
 import checkers.inference.solver.MaxSat2TypeSolver;
-import checkers.inference.solver.SolverEngine;
 import checkers.inference.test.CFInferenceTest;
 import org.checkerframework.framework.test.TestUtilities;
 import org.checkerframework.javacutil.Pair;
@@ -20,7 +19,7 @@ public class OsTrustedTest extends CFInferenceTest {
 
     @Override
     public Pair<String, List<String>> getSolverNameAndOptions() {
-        return Pair.<String, List<String>>of(SolverEngine.class.getCanonicalName(), new ArrayList<String>());
+        return Pair.<String, List<String>>of(MaxSat2TypeSolver.class.getCanonicalName(), new ArrayList<String>());
     }
 
     @Parameters

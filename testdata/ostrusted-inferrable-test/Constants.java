d260155a1e31d3715d5f9e1313d91f8b370a8ffd
d367wang <d367wang@uwaterloo.ca>
2021-11-02 11:38:02 -0400

    fix crash when interface contains array

---
+++ testdata/ostrusted-inferrable-test/Constants.java
@@ -0,0 +1,9 @@
+public interface Constants {
+    String[] ACCESS_NAMES = {
+        "public", "private", "protected", "static", "final", "synchronized",
+        "volatile", "transient", "native", "interface", "abstract", "strictfp",
+        "synthetic", "annotation", "enum"
+
+    };
+}
+

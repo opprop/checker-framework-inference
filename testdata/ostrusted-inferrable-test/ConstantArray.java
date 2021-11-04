049f4e2d97fc0efb274f23bc2dfa3f7b62431df8
d367wang <55197967+d367wang@users.noreply.github.com>
2021-11-04 12:53:08 -0400

    Update testdata/ostrusted-inferrable-test/ConstantArray.java

--- testdata/ostrusted-inferrable-test/ConstantArray.java
+++ testdata/ostrusted-inferrable-test/ConstantArray.java
@@ -1,6 +1,6 @@
 // The test case in https://github.com/opprop/checker-framework-inference/pull/366
 
-public interface Constants {
+public interface ConstantArray {
     String[] ACCESS_NAMES = {
         "public", "private", "protected", "static", "final", "synchronized",
         "volatile", "transient", "native", "interface", "abstract", "strictfp",

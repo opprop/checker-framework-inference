a96f7036979e40c130d042427721dc091780a45a
d367wang <d367wang@uwaterloo.ca>
2022-02-20 21:03:41 -0500

    update test case Anonymous.java

--- testdata/ostrusted-inferrable-test/Anonymous.java
+++ testdata/ostrusted-inferrable-test/Anonymous.java
@@ -1,15 +1,9 @@
-import ostrusted.qual.OsTrusted;
 import ostrusted.qual.OsUntrusted;
 
 class Anonymous {
-
-    @OsTrusted A foo() {
-        A a1 = new @OsUntrusted A() {};
-        A a2 = a1;
-        // :: error: (return.type.incompatible)
-        return a2;
-    }
+    A a1 = new @OsUntrusted A() {};
 }
+
 
 class A {}
 

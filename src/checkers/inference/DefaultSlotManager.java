016b3f9a28be9670f3255ede91e5e86219f76f18
Werner Dietl <wdietl@gmail.com>
2022-03-01 16:31:10 -0500

    Fix imports

--- src/checkers/inference/DefaultSlotManager.java
+++ src/checkers/inference/DefaultSlotManager.java
@@ -13,6 +13,7 @@ import org.checkerframework.framework.type.AnnotatedTypeMirror;
 import org.checkerframework.javacutil.AnnotationBuilder;
 import org.checkerframework.javacutil.AnnotationUtils;
 import org.checkerframework.javacutil.BugInCF;
+import org.checkerframework.javacutil.TreeUtils;
 import org.checkerframework.javacutil.TypeKindUtils;
 import org.checkerframework.javacutil.TypesUtils;
 

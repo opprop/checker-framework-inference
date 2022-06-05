08711b7217a1c65ee536a1a1513fe60a255a081d
Alex Liu <a299liu@uwaterloo.ca>
2022-06-05 15:49:41 -0400

    keep ub/lb of typevariable annotation in ExistentialVariables

--- src/checkers/inference/util/InferenceUtil.java
+++ src/checkers/inference/util/InferenceUtil.java
@@ -44,6 +44,14 @@ public class InferenceUtil {
         return oldAnnos;
     }
 
+    public static void removePrimaryTypeVariableAnnotation(AnnotatedTypeVariable atv, AnnotationMirror potentialVarAnno)  {
+        AnnotationMirror ub = atv.getUpperBound().getAnnotationInHierarchy(potentialVarAnno);
+        AnnotationMirror lb = atv.getLowerBound().getAnnotationInHierarchy(potentialVarAnno);
+        atv.removeAnnotation(potentialVarAnno);
+        atv.getUpperBound().addAnnotation(ub);
+        atv.getLowerBound().addAnnotation(lb);
+    }
+
     /**
      * TODO: This method is similar to the code in https://github.com/eisop/checker-framework
      * /blob/6f12277290642f8fb89a5c614b31fe419eb0a7b1/framework/src/main/java/

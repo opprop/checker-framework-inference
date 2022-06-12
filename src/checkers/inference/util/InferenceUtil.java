451983785fff7957201cd18e4e17d52528f87771
Alex Liu <a299liu@uwaterloo.ca>
2022-06-12 15:59:05 -0400

    refine code

--- src/checkers/inference/util/InferenceUtil.java
+++ src/checkers/inference/util/InferenceUtil.java
@@ -44,6 +44,12 @@ public class InferenceUtil {
         return oldAnnos;
     }
 
+    /**
+     * TODO: This method is similar to the code in https://github.com/eisop/checker-framework
+     * /blob/6f12277290642f8fb89a5c614b31fe419eb0a7b1/framework/src/main/java/
+     * org/checkerframework/framework/type/DefaultInferredTypesApplier.java#L134,
+     * it should be cleaned up when the code of this link is improved.
+     */
     public static void removePrimaryTypeVariableAnnotation(AnnotatedTypeVariable atv, AnnotationMirror potentialVarAnno)  {
         AnnotationMirror ub = atv.getUpperBound().getAnnotationInHierarchy(potentialVarAnno);
         AnnotationMirror lb = atv.getLowerBound().getAnnotationInHierarchy(potentialVarAnno);

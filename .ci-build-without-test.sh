13f832c7d66dbe2e2273a60d3c62babfbce03e33
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2024-03-29 15:18:42 -0400

    Clone and build annotation-tool in opprop

--- .ci-build-without-test.sh
+++ .ci-build-without-test.sh
@@ -23,11 +23,11 @@ export AFU="${AFU:-../annotation-tools/annotation-file-utilities}"
 # Don't use `AT=${AFU}/..` which causes a git failure.
 AT=$(dirname "${AFU}")
 
-## Build annotation-tools (Annotation File Utilities)
-#/tmp/plume-scripts/git-clone-related opprop annotation-tools "${AT}"
-#if [ ! -d ../annotation-tools ] ; then
-# ln -s "${AT}" ../annotation-tools
-#fi
+# Build annotation-tools (Annotation File Utilities)
+/tmp/plume-scripts/git-clone-related opprop annotation-tools "${AT}"
+if [ ! -d ../annotation-tools ] ; then
+ ln -s "${AT}" ../annotation-tools
+fi
 
 echo "Running:  (cd ${AT} && ./.build-without-test.sh)"
 (cd "${AT}" && ./.build-without-test.sh)
